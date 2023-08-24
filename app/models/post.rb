99c65ae22c94d2547353fd363d6a3f78b8fe09fd
Ankit <ankit26kushwa@gmail.com>
2023-08-25 01:06:53 +0530

    Fix typo error in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
 
-  after_save :update_user_posts_counter
+  after_save :update_posts_counter
 
-  def update_post_counter
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
