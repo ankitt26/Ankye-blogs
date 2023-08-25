ba3147df7b500c21fea8a8a9d1762da1723d322d
Ankit <ankit26kushwa@gmail.com>
2023-08-25 16:52:00 +0530

    Fix Typo in user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counters, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counters, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
 
