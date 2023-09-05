acbfd6b44edcda14f9b3e1c838ada0b8df78d8f0
Ankit <ankit26kushwa@gmail.com>
2023-09-06 00:10:51 +0530

    Change the expect body and user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+
   describe 'GET /users' do
     it 'responds with a successful status, correct template, and placeholder text' do
       get '/users'
-
+  
       expect(response).to have_http_status(200)
       expect(response).to render_template(:index)
-      expect(response.body).to include('Here is list of users')
+      expect(response.body).to include('Please add users first')
     end
   end
-
+  
   describe 'GET /users/:id' do
     it 'responds with a successful status, correct template, and placeholder text' do
+      user = User.create(id: 1, name: 'Test User') # Create a user with ID 1
       get '/users/1'
-
+  
       expect(response).to have_http_status(200)
       expect(response).to render_template(:show)
-      expect(response.body).to include('Here is details for a given user')
+      expect(response.body).to include('Test User') # Update this expectation to match the user's name or other relevant content.
     end
   end
 end
