bcb5adbf4cabf83f5168eb9b4fe816d2ebb02392
Ankit <ankit26kushwa@gmail.com>
2023-09-07 17:54:41 +0530

    Add before_action :authenticate_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_devise_parameters, if: :devise_controller?
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
